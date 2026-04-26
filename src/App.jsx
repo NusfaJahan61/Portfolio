3477419f506d47d784e8b24286ad51d7c17036dd
NusfaJahan61 <nusfaikra@gmail.com>
2026-04-26 23:10:25 +0600

    Navbar 2.0 updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <main className="overflow-x-hidden w-screen h-auto bg-[#e9e9e0]">
+    <main className="overflow-x-hidden w-screen h-auto bg-teal-900">
       <Routes>
         <Route path="/" element={<Navbar />} />
       </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { LuUserRound } from "react-icons/lu";
 import { CiGrid42 } from "react-icons/ci";
 import { LuPhone } from "react-icons/lu";
 import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 import { CiDark } from "react-icons/ci";
 import { CiLight } from "react-icons/ci";
 
@@ -29,7 +30,6 @@ const Navbar = () => {
   // ---------------------------------------------------State for theme toggle
 
   const [theme, setTheme] = useState("light");
-
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme)
@@ -39,68 +39,84 @@ const Navbar = () => {
   // ---------------------------------------------------State for sidebar toggle
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  // ---------------------------------------------------State for Minimizing Sidenavbar
+
+  const [minimize, setMinimize] = useState(false);
+  const toggleMinimize = () => {
+    setMinimize(!minimize)
+  }
+
   return (
-    <aside className="navbar w-[20dvw] h-screen flex flex-col justify-between bg-white">
+    <aside className="navbar w-[20dvw] h-screen flex flex-col justify-between  bg-[#e9e9e0] shadow-xl shadow-teal-300 text-teal-800">
 
-      {/* ----------SIDEBAR HEADER---------- */}
+      <div className="flex flex-col gap-5">
+        {/* ----------SIDEBAR HEADER---------- */}
 
-      <div className="sidebarheader flex flex-col justify-center items-center my-10 gap-2">
-        <img className="rounded-full size-40 object-cover shadow-xl" src={MY_INFO.picture} alt="image" />
-        <h1 className="text-2xl text-neutral-700">Nusfa Jahan Ikra</h1>
-        <p className="text-neutral-600 ">Hello! I'm a professional frontEnd Web Developer.</p>
-        <button><FaChevronLeft /></button>
+        <div className="sidebarheader flex flex-col justify-center items-center my-10 gap-2">
+          <img className="rounded-full size-40 object-cover shadow-sm shadow-teal-800" src={MY_INFO.picture} alt="image" />
+          <h1 className="text-2xl ">Nusfa Jahan Ikra</h1>
+          <p className=" ">Hello! I'm a FrontEnd Web Developer.</p>
+        </div>
+
+        {/* ---------MENU OPTIONS---------- */}
+
+        <ul className="p-5 flex flex-col gap-3 ">
+          <li>
+            <a className=" navMenu shadow-sm shadow-teal-700 active:bg-teal-700 active:text-teal-100 active:shadow-teal-300 flex flex-row items-center justify-start gap-3 rounded-full p-3 " href="#">
+              <span className="rounded-full text-2xl p-2 shadow-md transition-all ease-in-out "><CiHome /></span>
+              <span className="">Home</span>
+            </a>
+          </li>
+
+          <li>
+            <a className=" navMenu shadow-sm shadow-teal-700 active:bg-teal-700 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
+              <span className="rounded-full text-2xl p-2 shadow-md transition-all ease-in-out"><LuUserRound /></span>
+              <span>About Me</span>
+            </a>
+          </li>
+
+          <li>
+            <a className=" navMenu shadow-sm shadow-teal-700 active:bg-teal-700 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
+              <span className="rounded-full text-2xl p-2 shadow-md transition-all ease-in-out"><CiGrid42 /></span>
+              <span>Projects</span>
+            </a>
+          </li>
+          <li>
+            <a className=" navMenu shadow-sm shadow-teal-700 active:bg-teal-700 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
+              <span className="rounded-full text-2xl p-2 shadow-md transition-all ease-in-out"><MdOutlineVerifiedUser /></span>
+              <span>Certifications</span>
+            </a>
+          </li>
+          <li>
+            <a className=" navMenu shadow-sm shadow-teal-700 active:bg-teal-700 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
+              <span className="rounded-full text-2xl p-2 shadow-md transition-all ease-in-out"><LuPhone /></span>
+              <span>Contact</span>
+            </a>
+          </li>
+
+        </ul>
+
       </div>
 
-      {/* ---------MENU OPTIONS---------- */}
+      {/* -------MODE TOGGLE------- */}
+      <div className="bg-[#dadad1] flex flex-row items-center justify-between s px-5 ">
 
-      <ul className="p-5 flex flex-col gap-3 ">
-        <li>
-          <a className=" navMenu shadow-md active:bg-teal-600 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
-            <span className="rounded-full text-2xl p-2 shadow-md hover:text-teal-300 transition-all ease-in-out font-bold"><CiHome /></span>
-            <span className="">Home</span>
-          </a>
-        </li>
+        <div className="flex items-center">
+          <button className="bg-[#e9e9e0] font-bold rounded-full text-2xl p-3 m-2 shadow-md  hover:scale-105 transition-all ease-in-out cursor-pointer active:bg-neutral-400" onClick={toggleTheme}>
+            {theme === "light" ? <CiDark /> : <CiLight />}
+          </button>
+          <p className="ml-1 text-sm">{theme === "light" ? "Dark Mode" : "Light Mode"}</p>
+        </div>
 
-        <li>
-          <a className=" navMenu shadow-md active:bg-teal-600 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
-            <span className="rounded-full text-2xl p-2 shadow-md hover:text-teal-300 transition-all ease-in-out font-bold"><LuUserRound /></span>
-            <span>About Me</span>
-          </a>
-        </li>
-
-        <li>
-          <a className=" navMenu shadow-md active:bg-teal-600 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
-            <span className="rounded-full text-2xl p-2 shadow-md hover:text-teal-300 transition-all ease-in-out font-bold"><CiGrid42 /></span>
-            <span>Projects</span>
-          </a>
-        </li>
-        <li>
-          <a className=" navMenu shadow-md active:bg-teal-600 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
-            <span className="rounded-full text-2xl p-2 shadow-md hover:text-teal-300 transition-all ease-in-out font-bold"><MdOutlineVerifiedUser /></span>
-            <span>Certifications</span>
-          </a>
-        </li>
-        <li>
-          <a className=" navMenu shadow-md active:bg-teal-600 flex flex-row items-center justify-start gap-3 rounded-full p-3" href="#">
-            <span className="rounded-full text-2xl p-2 shadow-md hover:text-teal-300 transition-all ease-in-out font-bold"><LuPhone /></span>
-            <span>Contact</span>
-          </a>
-        </li>
-
-      </ul>
-
-      {/* -------SIDEBAR FOOTER------- */}
-      <div className="flex justify-between items-center px-5 rounded-full shadow-md mx-5 mb-5">
-        <p>{theme === "light" ? "Dark Mode" : "Light Mode"}</p>
-        <button className="rounded-full text-2xl p-3 m-3 shadow-md shadow-teal-300 hover:text-teal-300 transition-all ease-in-out cursor-pointer active:bg-neutral-400" onClick={toggleTheme}>
-          {theme === "light" ? <CiDark /> : <CiLight />}
-        </button>
+        <div className="flex justify-end m-3">
+          <button className="text-xl p-3 rounded-full bg-[#e9e9e0]" onClick={toggleMinimize}>{!minimize ? <FaChevronLeft /> : <FaChevronRight />}</button>
+        </div>
       </div>
+
 
     </aside>
   )
